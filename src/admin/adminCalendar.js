68adf3e7d1cf5c5f48b49e8eb49d3aa00b716da2
sean-one <basement365@gmail.com>
2019-05-06 12:30:00 -0700

    still working on logic for admin cal

--- src/admin/adminCalendar.js
+++ src/admin/adminCalendar.js
@@ -3,22 +3,30 @@ import React, { Component } from 'react';
 class AdminCalendar extends Component {
     constructor() {
         super();
+        this.state = {
+            today: new Date(),
+            firstDay: this.getFirstDay()
+        }
     }
 
-    componentDidMount() {
+    getFirstDay(){
         const today = new Date();
-        today.toDateString();
-        // console.log(`${today.getDay()}, ${today.getMonth()}, ${today.getDate()}, ${today.getFullYear()}`);
         const firstDay = new Date(`${today.getMonth() + 1}/1/${today.getFullYear()}`);
         const days = firstDay.getDay();
         firstDay.setDate( firstDay.getDate() - days);
+        // return firstDay;
         console.log(firstDay.toDateString());
+    }
+
+    getCurrentMonth(month) {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return months[month];
     }
 
     render() {
         return (
             <div>
-                <h1>calendar page</h1>
+                <h1>{this.getCurrentMonth(this.state.today.getMonth())}</h1>
             </div>
         );
     }
