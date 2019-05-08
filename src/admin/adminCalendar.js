00ce56eace58417eebe7972d1d575375c581896f
SeanOne <basement365@gmail.com>
2019-05-07 22:26:07 -0700

    added render for the days of the week

--- src/admin/adminCalendar.js
+++ src/admin/adminCalendar.js
@@ -28,12 +28,39 @@ class AdminCalendar extends Component {
             </div>
         );
     }
-    renderDays() {}
+
+    renderDays() {
+        const dateFormat = 'dddd';
+        const days = [];
+
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className='col col-center' key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+
+        return <div className="days row">{days}</div>;
+    }
     renderCells() {}
 
     onDateClick = day => {};
-    nextMonth = () => {};
-    prevMonth = () => {};
+    
+    nextMonth = () => {
+        this.setState({
+            currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+        });
+    };
+
+    prevMonth = () => {
+        this.setState({
+            currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+        });
+    };
+
     render() {
         return (
             <div className="calendar">
