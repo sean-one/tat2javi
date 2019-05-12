b2c1453ec0afcb5e13d8897feb2143ca698d7abe
SeanOne <basement365@gmail.com>
2019-05-11 18:58:39 -0700

    starting to layout the events form

--- src/admin/adminEvents.js
+++ src/admin/adminEvents.js
@@ -6,7 +6,22 @@ class AdminEvents extends Component {
             <div>
                 <h1>events posting page</h1>
                 <form>
-                    
+                    <div>
+                        <p>Event Name:</p><input type="text" name="event_name" />
+                    </div>
+                    <div>
+                        <p>Date Start:</p><input type="text" name="event_startDate"/>
+                        <p>Date End:</p><input type="text" name="event_endDate"/>
+                    </div>
+                    <p>Event Discription:</p>
+                    <textarea rows="10" cols="30" name="event_details"/>
+                    <div>
+                        <p>Event Link:</p><input type="text" name="event_link" />
+                    </div>
+                    <div>
+                        <p>Image Link:</p><input type="text" name="event_image" />
+                    </div>
+                    <button type="submit" value="submit">Submit</button>
                 </form>
             </div>
         );
