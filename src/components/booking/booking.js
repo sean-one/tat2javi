2c39e9c4a560458d27217db69a09e2259e0aec92
SeanOne <basement365@gmail.com>
2019-12-21 16:22:42 -0800

    started the appointment form

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -10,6 +10,14 @@ const Booking = (props) => {
                 <div className='pageTitle'>
                     <h1>Appointments</h1>
                 </div>
+                <form>
+                    <input name='firstName' type='text' value='' placeholder='First Name' />
+                    <input name='lastName' type='text' value='' placeholder='Last Name' />
+                    <input name='phone' type='text' value='' placeholder='(123)456-7890' />
+                    <input name='email' type='text' value='' placeholder='Email' />
+                    <input name='placement' type='text' value='' placeholder='Tattoo body placement' />
+                    <input name='description' type='text' value='' placeholder='Describe your tattoo idea' />
+                </form>
             </div>
         </div>
     );
