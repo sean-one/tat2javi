f6bdf9e7cfbcaa8d0d0310a70c117f274c1c627b
sean-one <basement365@gmail.com>
2023-05-06 20:44:41 -0700

    added toast to booking form submission success

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import axios from 'axios';
@@ -8,8 +11,7 @@ import { v4 as uuidv4 } from 'uuid'
 // styling
 const BookingStyles = styled.div`
     .booking {
-        padding: 1rem 0.25rem;
-        border: 1px solid red;
+        padding: 2.5rem 0.25rem;
     }
 
     .bookingForm {
@@ -63,32 +65,35 @@ const BookingStyles = styled.div`
 
 
 const Booking = (props) => {
-    const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm({
+    let navigate = useNavigate()
+    const { register, handleSubmit, setError, clearErrors, reset, formState: { errors } } = useForm({
         mode: 'onBlur',
     })
 
     const submitAppointment = async (data) => {
-        console.log(data.referenceImage)
         try {
-            const requiredFields = ['clientname', 'clientphone', 'clientemail', 'clientdescription']
+            // set up required fields and error messages
+            const requiredFields = ['clientname', 'clientphone', 'clientdescription']
             const errorMessages = {
                 clientname: 'name is required',
                 clientphone: 'contact number is required',
                 clientdescription: 'tattoo description is required',
             }
-
+            
+            // check for required fields, all fields requied except email
             requiredFields.forEach((field) => {
-                if(!data[field] && (field !== 'email')) {
+                if(!data[field]) {
                     setError(field, { message: errorMessages[field] })
                 }
             })
 
+            // check for required image
             if(data.referenceImage.length <= 0) {
                 setError('referenceImage', { message: 'reference image is required' })
             }
 
+            // set up s3 for image upload
             const imageToUpload = data.referenceImage[0]
-
             const s3 = new S3Client({
                 region: process.env.REACT_APP_AWS_REGION,
                 credentials: {
@@ -97,31 +102,44 @@ const Booking = (props) => {
                 },
             });
 
+            // create image upload detilas
             const fileExtension = imageToUpload.name.split('.').pop();
             const fileName = `${data.clientname}_${uuidv4()}.${fileExtension}`;
-
             const params = {
                 Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
                 Key: fileName,
                 Body: imageToUpload,
                 ContentType: imageToUpload.type,
-                // ACL: 'public-read',
             };
 
+            // send image to s3 and create the image s3 url
             const command = new PutObjectCommand(params);
             await s3.send(command);
             const url = `${process.env.REACT_APP_AWS_IMAGE_LINK}${fileName}`;
 
+            // remove the file from the data object before sending to lambda
             delete data['referenceImage']
             data['imagelink'] = url
 
+            // send to lambda function, sending an email to booking
             const response = await axios({
                 method : 'post',
                 url : process.env.REACT_APP_AWS_LAMBDA,
                 data : {...data},
             })
 
-            console.log(response)
+            if(response.status === 200) {
+                reset()
+                toast.success('Thank you for your message!  You should be contacted soon', {
+                    position: toast.POSITION.TOP_CENTER,
+                    autoClose: 3000 // 3 seconds
+                });
+                setTimeout(() => {
+                    navigate('/tat2javi')
+                }, 3000)
+
+            }
+            console.log(response.status)
 
             
         } catch(error) {
@@ -133,7 +151,7 @@ const Booking = (props) => {
     return (
         <BookingStyles>
             <div className='booking'>
-                <p>Do you have a tattoo idea that you would like </p>
+                <p>Do you have a tattoo idea that you would like</p>
                 <form onSubmit={handleSubmit(submitAppointment)} className='bookingForm' encType='multipart/form-data'>
                     
                     <input
@@ -179,6 +197,7 @@ const Booking = (props) => {
 
                     <button type='submit'>Submit</button>
                 </form>
+                <ToastContainer />
             </div>
         </BookingStyles>
     );
