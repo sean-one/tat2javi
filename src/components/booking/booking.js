eee21e869831992f967f783f7879885fe03d6ee5
sean-one <basement365@gmail.com>
2023-05-18 22:53:02 -0700

    fixed booking form errors

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
@@ -69,68 +69,79 @@ const BookingStyles = styled.div`
         padding: 1rem 2rem;
         border-radius: 5px;
     }
+
+    .disabledButton {
+        cursor: not-allowed;
+        background-color: red;
+    }
 `;
 
 
 const Booking = (props) => {
     let navigate = useNavigate();
 
-    const { handleSubmit, control, setError, clearErrors, reset, formState: { errors } } = useForm({
+    const { register, handleSubmit, clearErrors, reset, formState: { errors, isDirty } } = useForm({
         mode: 'onBlur',
-        // resolver: yupResolver(bookingSchema),
+        excludeEmptyString: true,
+        shouldFocusError: false,
+        shouldDisplayError: true,
     });
 
+    const validateImage = (file) => {
+        if (file && file[0]) {
+        const validTypes = ['image/jpeg', 'image/png', 'image/webp'];
+        const validSize = 5 * 1024 * 1024; // 5MB
+
+        if (!validTypes.includes(file[0].type)) {
+            return 'Only JPG, JPEG, PNG, and WEBP file types are allowed.';
+        }
+
+        if (file[0].size > validSize) {
+            return 'The file size should be smaller than 5MB.';
+        }
+        }
+
+        return true;
+    };
+
     const submitAppointment = async (data) => {
-        console.log('inside submit')
         try {
-            // set up required fields and error messages
-            const requiredFields = ['clientname', 'clientphone', 'clientdescription']
-            const errorMessages = {
-                clientname: 'name is required',
-                clientphone: 'contact number is required',
-                clientdescription: 'tattoo description is required',
-            }
             
-            // check for required fields, all fields requied except email
-            requiredFields.forEach((field) => {
-                if(!data[field]) {
-                    setError(field, { message: errorMessages[field] })
-                }
-            })
+            if(data.referenceImage.length > 0) {
+                // set up s3 for image upload
+                const imageToUpload = data.referenceImage[0]
+                const s3 = new S3Client({
+                    region: process.env.REACT_APP_AWS_REGION,
+                    credentials: {
+                        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+                    },
+                });
 
-            // check for required image
-            if(data.referenceImage.length <= 0) {
-                setError('referenceImage', { message: 'reference image is required' })
+                // create image upload detilas
+                const fileExtension = imageToUpload.name.split('.').pop();
+                const fileName = `${data.clientname}_${uuidv4()}.${fileExtension}`;
+                const params = {
+                    Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+                    Key: fileName,
+                    Body: imageToUpload,
+                    ContentType: imageToUpload.type,
+                };
+
+                // send image to s3 and create the image s3 url
+                const command = new PutObjectCommand(params);
+                await s3.send(command);
+                const url = `${process.env.REACT_APP_AWS_IMAGE_LINK}${fileName}`;
+                
+                // set the s3 url to the imagelink
+                data['imagelink'] = url
+
+            } else {
+                data['imagelink'] = 'no image attached'
             }
-
-            // set up s3 for image upload
-            const imageToUpload = data.referenceImage[0]
-            const s3 = new S3Client({
-                region: process.env.REACT_APP_AWS_REGION,
-                credentials: {
-                    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-                    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-                },
-            });
-
-            // create image upload detilas
-            const fileExtension = imageToUpload.name.split('.').pop();
-            const fileName = `${data.clientname}_${uuidv4()}.${fileExtension}`;
-            const params = {
-                Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
-                Key: fileName,
-                Body: imageToUpload,
-                ContentType: imageToUpload.type,
-            };
-
-            // send image to s3 and create the image s3 url
-            const command = new PutObjectCommand(params);
-            await s3.send(command);
-            const url = `${process.env.REACT_APP_AWS_IMAGE_LINK}${fileName}`;
 
             // remove the file from the data object before sending to lambda
             delete data['referenceImage']
-            data['imagelink'] = url
 
             // send to lambda function, sending an email to booking
             const response = await axios({
@@ -157,6 +168,7 @@ const Booking = (props) => {
             }
             
         } catch(error) {
+            console.log(error)
             if(error.name === 'TypeError') {
                 toast.error('Error connecting to server for file upload', {
                     position: toast.POSITION.TOP_CENTER,
@@ -181,114 +193,79 @@ const Booking = (props) => {
     }
 
 
-    console.log('errors')
-    console.log(errors)
     return (
         <BookingStyles>
             <div className='booking'>
                 <p>Send your contact and tattoo idea, and we can begin to bring your vision to life</p>
                 <form onSubmit={handleSubmit(submitAppointment)} className='bookingForm' encType='multipart/form-data'>
                     
-                    <Controller name='clientname'
-                        control={control}
-                        defaultValue=''
-                        rules={{
-                            required: 'Please enter your name'
-                        }}
-                        render={({ field }) => (
-                            <input id='clientname'
-                                {...field}
-                                type='text'
-                                onFocus={() => clearErrors('clientname')}
-                                placeholder='Name'
-                            />
-                        )}
+                    <input {...register('clientname', {
+                        required: 'name field is required',
+                        minLength: {
+                            value: 3,
+                            message: 'must be at least 3 letters'
+                        },
+                        pattern: {
+                            value: /^[A-Za-z\s]+$/, // Regular expression for alphabetic characters and spaces
+                            message: 'name must only include letters and spaces'
+                        }
+                    })}
+                        type='text'
+                        onFocus={() => clearErrors('clientname')}
+                        placeholder='Name'
                     />
-                    {errors.clientname && <div className='formError'>{errors.clientname?.message}</div>}
+                    {errors.clientname ? <div className='formError'>{errors.clientname?.message}</div> : null}
                     
-                    <Controller name="clientphone"
-                        control={control}
-                        defaultValue=''
-                        rules={{
-                            required: 'Please enter your phone number',
-                            pattern: {
-                                value: /^\d{10}$/,
-                                message: "Please enter a valid phone number"
-                            }
-                        }}
-                        render={({ field }) => (
-                            <input id='clientphone'
-                                {...field}
-                                type='text'
-                                onFocus={() => clearErrors('clientphone')}
-                                placeholder='Phone'
-                            />
-                        )}
+                    <input {...register('clientphone', {
+                        required: 'need a number bud',
+                        pattern: {
+                            value: /^(\+?1[-.\s]?)?(\()?\d{3}(\))?[-.\s]?\d{3}[-.\s]?\d{4}$/, // US phone pattern validation
+                            message: 'Please enter a valid U.S. phone number'
+                        }
+                    })}
+                        type='tel'
+                        onFocus={() => clearErrors('clientphone')}
+                        placeholder='Phone'
                     />
                     {errors.clientphone && <div className='formError'>{errors.clientphone?.message}</div>}
 
-                    <Controller name="clientemail"
-                        control={control}
-                        defaultValue=''
-                        rules={{
-                            pattern: {
-                                value: /\S+@\S+\.\S+/,
-                                message: "Please enter a valid email address"
-                            }
-                        }}
-                        render={({ field }) => (
-                            <input id='clientemail'
-                                {...field}
-                                type='email'
-                                onFocus={() => clearErrors('clientphone')}
-                                placeholder='Email'
-                            />
-                        )}
-                        
+                    <input {...register('clientemail', {
+                        pattern: {
+                            value: /^\S+@\S+$/i, // Email pattern validation
+                            message: 'Please enter a valid email address'
+                        }
+                    })}
+                        type='email'
+                        onFocus={() => clearErrors('clientemail')}
+                        placeholder='Email'
                     />
                     {errors.clientemail && <div className='formError'>{errors.clientemail?.message}</div>}
 
-                    <Controller name="clientdescription"
-                        control={control}
-                        defaultValue=''
-                        rules={{ required: 'Please include tattoo description and location' }}
-                        render={({ field }) => (
-                            <textarea id='clientdescription'
-                                {...field}
-                                onFocus={() => clearErrors('clientdescription')}
-                                rows='10'
-                                placeholder='Please describe your idea for a tattoo and your ideas for the location of the tattoo'
-                            />
-                        )}
+                    <textarea {...register('clientdescription', {
+                        required: 'a description of your tattoo and placement is required',
+                        minLength: {
+                            value: 20,
+                            message: 'more detailed information is needed'
+                        }
+
+                    })}
+                        onFocus={() => clearErrors('clientdescription')}
+                        rows='10'
+                        placeholder='Please describe your idea for a tattoo and your ideas for the location of the tattoo'
                     />
                     {errors.clientdescription && <div className='formError'>{errors.clientdescription?.message}</div>}
 
-                    <Controller name="referenceImage"
-                        control={control}
-                        defaultValue={null}
-                        rules={{
-                            validate: {
-                                maxSize: (files) =>
-                                    !files || 
-                                    (files[0] && files[0].size <= 5 * 1024 * 1024 * 1024),
-                                allowedTypes: (files) =>
-                                    !files ||
-                                    (files[0] && /(\.jpg|\.jpeg|\.png|\.webp)$/i.test(files[0].name)),
-                            }
-                        }}
-                        render={({ field: { onChange } }) => (
-                            <input
-                                type='file'
-                                onChange={(e) => onChange(e.target.files)}
-                                onFocus={() => clearErrors('referenceImage')}
-                                accept='image/*'
-                            />
-                        )}
+                    <input {...register('referenceImage', {
+                        validate: validateImage
+                    })}
+                    name='referenceImage'
+                    type='file'
+                    onFocus={() => clearErrors('referenceImage')}
+                    accept='image/*'
                     />
-                    {(errors.referenceImage && (errors.referenceImage.type === 'maxSize')) && <div className='formError'>File size should be less than 5GB</div>}
-                    {(errors.referenceImage && (errors.referenceImage.type === 'allowedTypes')) && <div className='formError'>Only JPG, JPEG, PNG, and WebP files are allowed</div>}
+                    {errors.referenceImage && <div className='formError'>{errors.referenceImage?.message}</div>}
 
-                    <button type='submit'>Submit</button>
+                    <button className={`${!isDirty || Object.keys(errors).length > 0 ? 'disabledButton' : ''}`} type='submit' disabled={!isDirty || Object.keys(errors).length > 0}>Submit</button>
                 </form>
                 <ToastContainer />
             </div>
